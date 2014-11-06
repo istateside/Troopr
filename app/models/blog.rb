e40b39fd2ff3fef59214b0ac202d59f4472efc3a
istateside <kevin.r.fleischman@gmail.com>
2014-11-05 20:05:54 -0500

    Added default urls for avatars. no more black squares.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -13,11 +13,23 @@
 
 
 class Blog < ActiveRecord::Base
+  AVATAR_DEFAULTS= [
+    'https://www.filepicker.io/api/file/4jnwDuuUSo6nx0RgkaHN',
+    'https://www.filepicker.io/api/file/sTX6eUk1Tqq6UxIWtiB0',
+    'https://www.filepicker.io/api/file/MGh0U0GWRDa8HLKvteJ0',
+    'https://www.filepicker.io/api/file/0MqJ0DtzSz6QaYI1h7CG',
+    'https://www.filepicker.io/api/file/WdIeC3i1QXiFN9fDaQX1',
+    'https://www.filepicker.io/api/file/2bDOO6xVRpqbGEaz8UEP',
+    'https://www.filepicker.io/api/file/OTpSnIKZRheksHULnnXn',
+    'https://www.filepicker.io/api/file/RxffJqDTSCyw4R7hgQwj'
+  ]
+
+  before_save :check_filepicker
   validates :blogname,
     length: {
       in: 6..20, wrong_length: "Blogname must be between 6-20 characters"
       },
-    format: { without: /\s/, message: "No spaces in blognames, please!" } 
+    format: { without: /\s/, message: "No spaces in blognames, please!" }
 
   belongs_to :user
 
@@ -26,7 +38,7 @@ class Blog < ActiveRecord::Base
 
   has_many :posts, dependent: :destroy
 
-  has_many :own_reblogs,
+  has_many :own_reblogs,\
     class_name: "Reblog",
     foreign_key: :blog_id,
     inverse_of: :reblogger
@@ -46,7 +58,11 @@ class Blog < ActiveRecord::Base
 
   has_many :following, through: :follows, source: :target
   has_many :followers, through: :follows, source: :source
-
+  def check_filepicker
+    if self.filepicker_url.blank?
+      self.filepicker_url = AVATAR_DEFAULTS.sample
+    end
+  end
 
   def is_following?(blog)
     self.following.include?(blog)
